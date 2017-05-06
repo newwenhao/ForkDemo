ebcf1260ba5704046f9c391be91da648efbff090
wenhao <newwenhao@sina.com>
2017-05-06 21:22:48 +0800

    stu

---
+++ app/src/main/java/com/wtu/com/forkdemo/bean/stu.java
@@ -0,0 +1,17 @@
+package com.wtu.com.forkdemo.bean;
+
+/**
+ * Created by Brain on 2017/5/6.
+ */
+
+public class stu {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
